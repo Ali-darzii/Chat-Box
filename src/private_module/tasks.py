6ec8f342921960fb65a29a00beace58c1b6fc1db
Ali-darzii <ali.darzi.1354@gmail.com>
2025-06-28 00:20:18 +0330

    fea(celery): create private boxes of user after user created

---
+++ src/private_module/tasks.py
@@ -0,0 +1,33 @@
+from celery import shared_task
+import logging
+from django.db import IntegrityError as UniqueError
+
+
+logger = logging.getLogger("django")
+
+
+@shared_task(queue="private_boxes")
+def create_private_box(user_id:int):
+    """ Create private boxes after user created """
+    # circular import
+    from private_module.models import PrivateBox
+    from auth_module.models import User
+    logger.info(f"Creating private box for user {user_id} started ...")
+
+    all_users = User.objects.all().exclude(id=user_id)
+    private_boxes = []
+    for user in all_users:
+        private_boxes.append(
+            PrivateBox(first_user_id=user_id, second_user_id=user.id)
+        )
+    try:
+        PrivateBox.objects.bulk_create(private_boxes, ignore_conflicts=True)
+    except UniqueError as e:
+        logger.error(f"UniqueError first_user_id: {user_id} second_user_id:{user.id} \n\n {e}")
+
+    except Exception as e:
+        logger.critical(f"UnknownError first_user_id: {user_id} second_user_id:{user.id} \n\n {e}")
+
+    logger.info(f"Creating private box for user {user_id} Finished.")
+
+
--- src/run.sh
+++ src/run.sh
@@ -3,7 +3,8 @@
 pkill -f celery
 pkill -f runserver
 
-screen -mdS run python manage.py runserver 192.168.102.3:8080
+screen -mdS run python manage.py runserver 127.0.0.1:8000
 
 celery -A ChatBox purge -f
 screen -mdS send-sms celery -A ChatBox worker -Q send_sms -n send_sms.@%h -f ./log/python/celery/send_sms.log --concurrency=1 -l debug
+screen -mdS private_boxes celery -A ChatBox worker -Q private_boxes -n private_boxes.@%h -f ./log/python/celery/private_boxes.log --concurrency=1 -l debug
